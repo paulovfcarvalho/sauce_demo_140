e82d3d987f7bd5234198a9f825323d300d264d50
Paulo Carvalho <paulovfcarvalho@gmail.com>
2024-03-21 22:05:48 -0300

    Exemplos com tratamento por IF

--- features/steps/selecionar_produtos.py
+++ features/steps/selecionar_produtos.py
@@ -9,7 +9,7 @@ def step_impl(context):
     #Setup / Inicialização
     context.driver = webdriver.Chrome()             # instanciar o objeto do Selenium WebDriver especializado para o Chrome
     context.driver.maximize_window                  # maximizar a jenela do navegador
-    context.driver.implicitly_wait(1)               # esperar ate 5seg por qualquer elemento
+    context.driver.implicitly_wait(10)               # esperar ate 5seg por qualquer elemento
     #Passo em si
     context.driver.get('https://www.saucedemo.com')
 
@@ -21,22 +21,22 @@ def step_impl(context,usuario,senha):
     context.driver.find_element(By.ID,"login-button").click()
 
 # Preencher com usuário em branco e senha
-@when(u'escrevo os campos de login com o usuario  e senha {senha}')
+@when(u'preencho os campos de login com o usuario  e senha {senha}')
 def step_impl(context,senha):
     # não preenche o usuário
     context.driver.find_element(By.ID,"password").send_keys(senha)
     context.driver.find_element(By.ID,"login-button").click()
 
 # Preencher com usuário, mas deixar a senha em branco
-@when(u'escrevo os campos de login com o usuario {usuario} e senha ')
+@when(u'preencho os campos de login com o usuario {usuario} e senha ')
 def step_impl(context,usuario,senha):
     context.driver.find_element(By.ID,"user-name").send_keys(usuario)
     # não preenche a senha
     context.driver.find_element(By.ID,"login-button").click()
 
 # Clica no botão login sem ter preenchido o usuário e senha
-@when(u'escrevo os campos de login com o usuario {usuario} e senha ')
-def step_impl(context,usuario,senha):
+@when(u'preencho os campos de login com o usuario  e senha ')
+def step_impl(context):
     # não preenche usuário e senha
     context.driver.find_element(By.ID,"login-button").click()    
 
